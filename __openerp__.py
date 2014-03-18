73cc97a58cc6ea83d525975b8bec05db6a7fac39
Jérémie Gavrel <jeremie.gavrel@xcg-consulting.fr>
2014-03-18 10:53:20 +0100

    Initial commit.

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+{
+    'name': u"Asset Streamline",
+    'version': u"0.1",
+    'author': u"XCG Consulting",
+    'category': u"Custom Module",
+    'description': u"""Includes several integrity fixes and optimizations over
+    the standard module.
+    """,
+    'website': u"",
+    'depends': [
+        'base',
+        'account',
+        'account_asset'
+    ],
+    'data': [
+        'views/account_asset_view.xml',
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
