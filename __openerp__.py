a90fab1994be6cc624a61da548466ce7f9d29333
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-06-26 11:37:35 +0200

    Bumped to version 1.2.3 (dev)

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 ##############################################################################
 {
     'name': u"Asset Streamline",
-    'version': u"1.2.2",
+    'version': u"1.2.3",
     'author': u"XCG Consulting",
     'category': u"Custom Module",
     'description': u"""Includes several integrity fixes and optimizations over
