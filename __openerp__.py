fe03a0554c92d6d7140be2c9cc6e76ea391a2ca1
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-11-24 16:37:57 +0100

    Added missing information in __openerp__

--- __openerp__.py
+++ __openerp__.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 XCG Consulting
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ##############################################################################
 {
-    'name': u"Asset Streamline",
-    'version': "1.4.1",
-    'author': u"XCG Consulting",
-    'category': u"Custom Module",
-    'description': u"""Includes several integrity fixes and optimizations over
-    the standard module.
+    'name': "Asset Management Streamline",
+    'version': '1.5',
+    'author': "XCG Consulting",
+    'category': "Accounting & Finance",
+    'description': """Includes several integrity fixes and optimizations over
+    the standard Asset Management module.
     """,
-    'website': u"",
+    'website': 'http://www.openerp-experts.com/',
     'depends': [
         'base',
         'account_streamline',
